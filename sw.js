a607117b9bc7bd2e266408b4a281a06c1d64c56b
lambder-2025 <lalogabry3@gmail.com>
2025-09-16 19:47:13 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,16 @@
 const CACHE_NAME = 'loyalty-app-cache-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html'
-];
+const ASSETS_TO_CACHE = [ './', './index.html' ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache abierto');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
+  event.waitUntil( caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)) );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+  event.waitUntil( caches.keys().then(keyList => Promise.all(keyList.map(key => { if (key !== CACHE_NAME) return caches.delete(key); }))) );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  // Estrategia: Network falling back to cache
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+  event.respondWith( fetch(event.request).catch(() => caches.match(event.request)) );
 });
